b663ae2693e93fd369d93d38fcab05c43e802cd6
Luke Taher <luketaher@gmail.com>
2016-10-18 22:38:38 +0100

    Added operator evaluation

--- src/some_eval.ml
+++ src/some_eval.ml
@@ -7,6 +7,21 @@ type value =
 
 let store = Hashtbl.create 100
 
+let eval_op op e e' =
+	match (op, e, e') with 
+	  | (Add, Integer e, Integer e') -> Integer (e+e')
+	  | (Sub, Integer e, Integer e') -> Integer (e-e') 
+	  | (Mul, Integer e, Integer e') -> Integer (e*e') 
+	  | (Div, Integer e, Integer e') -> Integer (e/e')
+	  | (Eq, Integer e, Integer e') -> Bool (e==e')
+	  | (Le, Integer e, Integer e') -> Bool (e<e') 
+	  | (Ge, Integer e, Integer e') -> Bool (e>e') 
+	  | (Leq, Integer e, Integer e') -> Bool (e<=e') 
+	  | (Geq, Integer e, Integer e') -> Bool (e>=e')
+	  | (And, Bool e, Bool e') -> Bool (e&&e') 
+	  | (Or, Bool e, Bool e') -> Bool (e||e')
+	  | _ -> failwith "unable to match"
+
 let rec eval_exp_left = function
 	| Identifier s -> s
 	| _ -> failwith "unable to match"
@@ -19,6 +34,7 @@ let rec eval_exp = function
 					 let right = eval_exp e' in
 					 Hashtbl.replace store left right;
 					 Unit ()
+	| Operation (op, e, e') -> eval_op op (eval_exp e) (eval_exp e')
 	| Deref Identifier s -> Hashtbl.find store s
 	| Return e -> eval_exp e
 	| _ -> failwith "unable to match"
@@ -27,7 +43,7 @@ let rec eval_exp = function
 let eval_fundef (name, ps, exp) = eval_exp exp
 
 let rec eval_prog = function
-	| [] -> Integer 0
+	| [] -> Unit ()
 	| ("main", ps, exp)::xs -> eval_fundef ("main", ps, exp)
 	| x::xs -> eval_prog xs
 
