20381cb84e3f470e0c2c7a232de814ab3ee53194
Luke Taher <luketaher@gmail.com>
2016-11-09 22:46:08 +0000

    Added codegen for functions

--- src/some_cgen.ml
+++ src/some_cgen.ml
@@ -5,7 +5,6 @@ open Hashtbl
 let funs = Hashtbl.create 100
 
 (* Instruction Set *)
-(* let ram = Hashtbl.create 103  *)
 let acc = ref 0
 
 let lblno = ref 0
@@ -30,6 +29,8 @@ let op (op, addr1, addr2) = "\t" ^ (string_of_op op) ^ " r" ^ (string_of_int add
                             ", r" ^ (string_of_int addr2) ^ "\n"
                             |> Buffer.add_string code
 let st addr = "\tst r" ^ (string_of_int addr) ^ "\n" |> Buffer.add_string code
+let stbp () = "\tst bp" ^ "\n" |> Buffer.add_string code
+let ldbp () = "\tld bp" ^ "\n" |> Buffer.add_string code
 let ldc n = "\tldc " ^ (string_of_int n) ^ "\n" |> Buffer.add_string code
 let ld addr = "\tld r" ^ (string_of_int addr) ^ "\n" |> Buffer.add_string code
 let mv addr1 addr2 = "\tmv r" ^ (string_of_int addr1) ^
@@ -44,16 +45,17 @@ let cmp addr n = "\tcmp r" ^ (string_of_int addr) ^
                  ", " ^ (string_of_int n) ^ "\n"
                  |> Buffer.add_string code
 let jmp lbl = "\tjmp " ^ lbl ^ "\n" |> Buffer.add_string code
+let call lbl = "\tcall " ^ lbl ^ "\n" |> Buffer.add_string code
 let jmpz lbl = "\tjmpz " ^ lbl ^ "\n" |> Buffer.add_string code
 let printlbl lbl = lbl ^ ":\n" |> Buffer.add_string code
 
 (* Address generation *)
-let stack_base = ref 3
-let newaddr () = stack_base:=!stack_base+1; if !stack_base < 53 then !stack_base
+let stack_pointer = ref 3
+let newaddr () = stack_pointer:=!stack_pointer+1; if !stack_pointer < 53 then !stack_pointer
                                             else failwith "Buffer Overflow"
 
-let heap_base = ref 103
-let heapalloc () = heap_base:=!heap_base-1; if !heap_base > 52 then !heap_base
+let heap_pointer = ref 103
+let heapalloc () = heap_pointer:=!heap_pointer-1; if !heap_pointer > 52 then !heap_pointer
                                             else failwith "Buffer Overflow"
 
 (* Address lookup *)
@@ -72,9 +74,9 @@ let rec cgen_exp symt = function
   | Let (s, e, e') -> let addr1 = cgen_exp symt e in
                       let addr2 = cgen_exp ((s, addr1)::symt) e' in
                       mv addr1 addr2;
-                      stack_base := addr1;
+                      stack_pointer := addr1;
                       addr1
-  | New (s, e, e') -> let hbtemp = !heap_base in
+  | New (s, e, e') -> let hbtemp = !heap_pointer in
                       let addr1 = cgen_exp symt e in
                       let haddr = heapalloc() in
                       mv haddr addr1;
@@ -82,10 +84,10 @@ let rec cgen_exp symt = function
                       st addr1;
                       let addr2 = cgen_exp ((s, addr1)::symt) e' in
                       mv addr1 addr2;
-                      stack_base := addr1;
-                      heap_base := hbtemp;
+                      stack_pointer := addr1;
+                      heap_pointer := hbtemp;
                       addr1
-  (* | Application (s, args) -> inter_fundef (s, args) symt *)
+  | Application (s, args) -> cgen_fundef (s, args) symt
   | Identifier s -> let addr = lookup s symt in
                     let addr' = newaddr() in
                     mv addr' addr;
@@ -96,12 +98,12 @@ let rec cgen_exp symt = function
                    let addr2 = cgen_exp symt e' in
 									 ld addr1;
                    mvta addr2;
-                   stack_base := addr1;
+                   stack_pointer := addr1;
                    addr1
   | Operation (oper, e, e') -> let addr1 = cgen_exp symt e in
                                let addr2 = cgen_exp symt e' in
                                op (oper, addr1, addr2);
-                               stack_base := addr1;
+                               stack_pointer := addr1;
                                st addr1;
                                addr1
   | Negation e -> let addr = cgen_exp symt e in
@@ -116,12 +118,12 @@ let rec cgen_exp symt = function
                        jmpz lbl;
                        let addr2 = cgen_exp symt e' in
                        mv addr1 addr2;
-                       stack_base := addr1;
+                       stack_pointer := addr1;
                        jmp endlbl;
                        printlbl lbl;
                        let addr2 = cgen_exp symt e'' in
                        mv addr1 addr2;
-                       stack_base := addr1;
+                       stack_pointer := addr1;
                        printlbl endlbl;
                        addr1
   | While (e, e') ->  let addr1 = cgen_exp symt e in
@@ -146,36 +148,48 @@ let rec cgen_exp symt = function
   | _ -> failwith "Unable to Interpret"
 
 (* Function interpretation *)
-(* and inter_fundef (name, argvs) symt =
+and cgen_fundef (name, argvs) symt =
   let (args, exp) = try find funs name
                     with Not_found -> failwith ("Unable to match - Function definition "^ name ^" not found")(*lambda_fetch name env*) in
-  let stemp = !stack_base in
-  let htemp = !heap_base in
-  let res = cgen_exp (gen_stackframe args argvs symt) exp in
-  stack_base := stemp;
-  heap_base := htemp;
-  res *)
+  let addr1 = newaddr() in
+  let stemp = !stack_pointer in
+  let htemp = !heap_pointer in
+  let addr2 = newaddr() in
+  ldbp();
+  st addr1;
+  gen_stackframe args argvs symt;
+  ldc stemp;
+  stbp();
+  call name;
+  st addr2;
+  ld addr1;
+  stbp();
+  mv addr1 addr2;
+  stack_pointer := stemp;
+  heap_pointer := htemp;
+  addr1
 
 (* Stack Frame *)
-(* and gen_stackframe args argvs symt =
+and gen_stackframe args argvs symt =
   match (args, argvs) with
-  | (arg::args, argv::argvs) -> let addr1 = cgen_exp symt argv in
-                                (arg, addr1)::(gen_stackframe args argvs symt)
-  | _ -> [] *)
+  | (arg::args, argv::argvs) -> cgen_exp symt argv |> ignore;
+                                gen_stackframe args argvs symt
+  | _ -> ()
 
 let rec cgen_prog' = function
   | ("main", args, exp)::prog ->printlbl "_start";
+                                stack_pointer := 3;
                                 let addr = (cgen_exp [] exp) in
                                 ld addr;
                                 jmp "END";
                                 printlbl "END";
                                 "\tnop\n" |> Buffer.add_string code;
-                                Buffer.output_buffer stdout code;
                                 cgen_prog' prog
   | (name, args, exp)::prog ->  printlbl name;
-                                let addr = (cgen_exp [] exp) in
+                                stack_pointer := 3;
+                                let addr = (cgen_exp (List.map (fun arg -> (arg, (newaddr()))) args) exp) in
                                 ld addr;
-                                Buffer.output_buffer stdout code;
+                                "\tret\n" |> Buffer.add_string code;
                                 cgen_prog' prog
   | _ -> ()
 
@@ -183,6 +197,6 @@ let rec add_funs = function
   | (name, (args:string list), exp)::prog -> replace funs name (args, exp); add_funs prog
   | _ -> ()
 
-let rec cgen_prog prog = add_funs prog; cgen_prog' prog
+let rec cgen_prog prog = add_funs prog; cgen_prog' prog; Buffer.output_buffer stdout code
   (* | (name, args, exp)::prog -> replace funs name (args, exp); cgen_prog prog
 	| _ -> find ram (inter_fundef ("main", []) []) *)
